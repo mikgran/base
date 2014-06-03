d480a2e2777b596a0071b6bee3c5975627c2348e
Mika Granqvist <mika.granqvist@outlook.com>
2014-06-03 13:14:11 +0300

    Reformat of the server class.

--- reservation/src/main/java/mg/reservation/server/ReservationServer.java
+++ reservation/src/main/java/mg/reservation/server/ReservationServer.java
@@ -11,29 +11,26 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
-public class ReservationServer extends AbstractHandler
-{
+public class ReservationServer extends AbstractHandler {
 	public void handle(String target,
 			Request baseRequest,
 			HttpServletRequest request,
 			HttpServletResponse response)
-			throws IOException, ServletException
-	{
+			throws IOException, ServletException {
+
 		response.setContentType("text/html;charset=utf-8");
 		response.setStatus(HttpServletResponse.SC_OK);
 		baseRequest.setHandled(true);
 		response.getWriter().println("<h1>Message</h1>");
 	}
 
-	public static void main(String[] args) throws Exception
-	{
+	public static void main(String[] args) throws Exception {
 		Server server = new Server(8888);
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        
+		context.setContextPath("/");
+		server.setHandler(context);
+
 		server.start();
 		server.join();
-
 	}
 }
