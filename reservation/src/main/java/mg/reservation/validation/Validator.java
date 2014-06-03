31718f82001fe204dc2f76d7a3d6eb60aeda7611
Mika Granqvist <mika.granqvist@outlook.com>
2014-06-03 21:45:57 +0300

    Comment changes.

--- reservation/src/main/java/mg/reservation/validation/Validator.java
+++ reservation/src/main/java/mg/reservation/validation/Validator.java
@@ -7,41 +7,43 @@ import java.util.Map;
 import mg.reservation.validation.rule.ValidationRule;
 
 /**
- * Usage: new Validator().add("fieldName", fieldName, new ValidationRule()).validate();
- * 
- * Notes on argument validation:
- * - validation could be done at the end: the class using the parameter.
- * 
- * - if validation is done at the end, all the code that would fail because
- * 		of missing parameter is also run.
- * 
- * - side effects of the missing (null) parameter may lead to obscure error messages:
- * 		i.e. sql exception for bad sql query. Instead a meaningful "primary key
- * 		missing" would lead the developer to the cause of the error faster.
- * 
- * - side effect of validation is that the program flow breaks before any 
- * 		resource critical method calls have been made.
- * 
- * - parameter validation should be used mainly in interfaces where user
- * 		is not the author of the interface they are calling to allow better control
- * 		and understanding of the program: i.e. service and db classes.
- * 
- * - validating parameters is considered as protective coding and some believe 
- * 		that it should be avoided as excessive and unnessecary programming style.
- * 
- * - validating parameters is thought of as a step towards more complex and less 
- * 		understandable code: i.e. consider cyclomatic complexity 
- * 		(http://en.wikipedia.org/wiki/Cyclomatic_complexity) since it produces 
- * 		one more exit point for the method it is used in.
- * 
- * - validating complies to the Clean Code 'prefer exceptions to error codes' 
- * 		best practise.
- * 
- * - extra defects may be produced with adding improper prevalidation to a program. 
- * 
- * - this class exists only to demonstrate the use of extensible validation pattern.
- * 		Use Preconditions.checkNotNull (com.google.common.base) for an alternative.
+ * Usage: new Validator()
+ *            .add("fieldName", fieldName, new ValidationRule())
+ *            .validate();
  */
+/* Notes on argument validation:
+* - validation could be done at the end: the class using the parameter.
+* 
+* - if validation is done at the end, all the code that would fail because
+* 		of missing parameter is also run.
+* 
+* - side effects of the missing (null) parameter may lead to obscure error messages:
+* 		i.e. sql exception for bad sql query. Instead a meaningful "primary key
+* 		missing" would lead the developer to the cause of the error faster.
+* 
+* - side effect of validation is that the program flow breaks before any 
+* 		resource critical method calls have been made.
+* 
+* - parameter validation should be used mainly in interfaces where user
+* 		is not the author of the interface they are calling to allow better control
+* 		and understanding of the program: i.e. service and db classes.
+* 
+* - validating parameters is considered as protective coding and some believe 
+* 		that it should be avoided as excessive and unnessecary programming style.
+* 
+* - validating parameters is thought of as a step towards more complex and less 
+* 		understandable code: i.e. consider cyclomatic complexity 
+* 		(http://en.wikipedia.org/wiki/Cyclomatic_complexity) since it produces 
+* 		one more exit point for the method it is used in.
+* 
+* - validating complies to the Clean Code 'prefer exceptions to error codes' 
+* 		best practise.
+* 
+* - extra defects may be produced with adding improper prevalidation to a program. 
+* 
+* - this class exists only to demonstrate the use of extensible validation pattern.
+* 		Use Preconditions.checkNotNull (com.google.common.base) for an alternative.
+*/
 public class Validator {
 
 	private Map<String, Validatable> validatableObjects = new HashMap<String, Validatable>();
@@ -65,10 +67,11 @@ public class Validator {
 	 * If validation results in any of the parameters being invalid, an InvalidArgumentException 
 	 * will be thrown and a message comprised of the names of each validatable and their 
 	 * exception messages acquired from the validators.
-	 * @throws IllegalAccessException See Class.newInstance();
-	 * @throws InstantiationException See Class.newIsntance();
-	 * @throws IllegalArgumentException If there is no exceptionClass defined. Otherwise that type exception
-	 * is thrown.
+	 * 
+	 * Note the throwException method is called if any of the validators return a false.
+	 * Subclasses should override this to customize the behavior.
+	 * 
+	 * @throws IllegalArgumentException if any of the validators returned false. 
 	 */
 	public void validate() {
 
@@ -91,7 +94,6 @@ public class Validator {
 					exceptionMessage.append(entry.getKey());
 					exceptionMessage.append(" ");
 					exceptionMessage.append(rule.getMessage());
-					exceptionMessage.append(" ");
 				}
 			}
 		}
