340b61f1fe884602b39e7bf017cd747c0d80ebd5
Mika Granqvist <mika.granqvist@outlook.com>
2014-06-09 19:49:54 +0300

    Changed logging for listing reservations. Changed log4j config: added log level to the beginning.

--- reservation/src/main/java/mg/reservation/rest/ReservationResource.java
+++ reservation/src/main/java/mg/reservation/rest/ReservationResource.java
@@ -60,7 +60,7 @@ public class ReservationResource {
 	@Produces({ MediaType.APPLICATION_JSON })
 	public List<Reservation> listReservations(@QueryParam("start") String startTime, @QueryParam("end") String endTime) {
 
-		logger.info("start: ({}), end: ({})", startTime, endTime);
+		logger.info("listReservations(start:{}, end:{})", startTime, endTime);
 
 		new RestRequestParameterValidator()
 				.add("start", startTime, NOT_NEGATIVE_OR_ZERO_AS_STRING)
