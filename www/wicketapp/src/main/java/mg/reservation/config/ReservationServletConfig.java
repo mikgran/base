77006d3854f60b052ad3fa352e1455a5ad52b3da
Mika Granqvist <mika.granqvist@outlook.com>
2014-06-14 16:52:39 +0300

    Comment change.

--- www/wicketapp/src/main/java/mg/reservation/config/ReservationServletConfig.java
+++ www/wicketapp/src/main/java/mg/reservation/config/ReservationServletConfig.java
@@ -4,15 +4,11 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.servlet.GuiceServletContextListener;
 
+// accessed via <listener-class>mg.reservation.config.ReservationServletConfig</listener-class> in pom.xml
 public class ReservationServletConfig extends GuiceServletContextListener {
 
 	@Override
 	protected Injector getInjector() {
 		return Guice.createInjector(new ReservationServletModule());
 	}
-	
-
 }
- 
-
- 
