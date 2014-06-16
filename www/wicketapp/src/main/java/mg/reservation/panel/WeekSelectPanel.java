d2c8e7a13120e04146fecb68679d11716a189e6a
Mika Granqvist <mika.granqvist@outlook.com>
2014-06-16 10:42:47 +0300

    Added ajaxlink update for the reservations panel.
    Still missing:
    - generics in components
    - week handling functionality
    - tests
    - cleanup

--- www/wicketapp/src/main/java/mg/reservation/panel/WeekSelectPanel.java
+++ www/wicketapp/src/main/java/mg/reservation/panel/WeekSelectPanel.java
@@ -1,44 +1,60 @@
 package mg.reservation.panel;
 
+import mg.reservation.model.ReservationsModel;
+
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WeekSelectPanel extends Panel {
 
+	private static final long serialVersionUID = 612552405494581062L;
+	private Logger logger = LoggerFactory.getLogger(WeekSelectPanel.class);
 	private TextField<String> weekField;
+	private WebMarkupContainer parent;
+	private ReservationsModel reservationsModel;
 
-	public WeekSelectPanel(String id) {
+	public WeekSelectPanel(String id, ReservationsModel reservationsModel, final WebMarkupContainer parent) {
 		super(id);
+		this.reservationsModel = reservationsModel;
+		this.parent = parent;
 
 		weekField = new TextField<String>("week", Model.of(""));
 
 		Form form = getWeekSelectionForm();
-		form.add(new Label("currentWeek", new Model<String>(getCurrentWeekNumber())));
+		form.add(new Label("currentWeek", new Model<String>(reservationsModel.getSelectedWeekAsString())));
 		form.add(weekField);
+
+		form.add(new AjaxSubmitLink("ajaxSubmit") { // TODO generics
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form form) {
+				logger.info("Adding parent to be refreshed.");
+				target.add(parent);
+			}
+		});
 
 		add(form);
 	}
 
-	// TODO model type generics
+	// TODO generics
 	private Form getWeekSelectionForm() {
 		return new Form("weekSelection") {
 			private static final long serialVersionUID = -3756364422045777230L;
 
 			@Override
 			protected void onSubmit() {
-				System.out.println("form was submitted with " + weekField.getModelObject());
+				logger.info("form was submitted with {}", weekField.getModelObject()); // TODO remove
+				reservationsModel.setWeek(weekField.getModelObject());
 			}
 		};
-	}
-
-	private static final long serialVersionUID = 612552405494581062L;
-
-	private String getCurrentWeekNumber() {
-		return "" + new DateTime().weekOfWeekyear().get();
 	}
 
 }
