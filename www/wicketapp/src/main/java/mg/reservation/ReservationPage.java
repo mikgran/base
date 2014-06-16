b7a7a705b563a878a6e8ab12ab0d66e7c2b76c56
Mika Granqvist <mika.granqvist@outlook.com>
2014-06-16 10:40:20 +0300

    Added WMC for grouping the top level components.

--- www/wicketapp/src/main/java/mg/reservation/ReservationPage.java
+++ www/wicketapp/src/main/java/mg/reservation/ReservationPage.java
@@ -1,25 +1,42 @@
 package mg.reservation;
 
+import mg.reservation.model.ReservationsModel;
 import mg.reservation.panel.ReservationsPanel;
 import mg.reservation.panel.WeekSelectPanel;
+import mg.reservation.service.ReservationService;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import com.google.inject.Inject;
 
 public class ReservationPage extends WebPage {
 
 	private static final long serialVersionUID = -7385489983766355838L;
 
+	@Inject
+	private ReservationService reservationService;
+
 	private ReservationsPanel reservationsPanel;
 	private WeekSelectPanel weekSelectPanel;
+
+	private ReservationsModel reservationsModel;
 
 	public ReservationPage(final PageParameters parameters) {
 		super(parameters);
 
-		reservationsPanel = new ReservationsPanel("reservationsList");
-		weekSelectPanel = new WeekSelectPanel("weekSelect");
+		reservationsModel = new ReservationsModel(reservationService);
 
-		add(reservationsPanel);
-		add(weekSelectPanel);
+		WebMarkupContainer parent = new WebMarkupContainer("parent");
+		parent.setOutputMarkupId(true);
+
+		reservationsPanel = new ReservationsPanel("reservationsList", reservationsModel, parent);
+		weekSelectPanel = new WeekSelectPanel("weekSelect", reservationsModel, parent);
+
+		parent.add(reservationsPanel);
+		parent.add(weekSelectPanel);
+
+		add(parent);
 	}
 }
