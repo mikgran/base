e549e70ba39586dcc15a96830f88c3338fd8ab44
Mika Granqvist <mika.granqvist@outlook.com>
2014-06-20 13:51:52 +0300

    Added more localization strings for compontents. Cleanup: MainPage.

--- www/wicketapp2/src/main/java/mg/wicketapp2/config/WicketApplication.java
+++ www/wicketapp2/src/main/java/mg/wicketapp2/config/WicketApplication.java
@@ -6,24 +6,13 @@ import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.odlabs.wiquery.ui.themes.WiQueryCoreThemeResourceReference;
 
-/**
- * Application object for your web application.
- * If you want to run this application without deploying, run the Start class.
- * 
- * @see mg.wicketapp2.Start#main(String[])
- */
 public class WicketApplication extends WebApplication {
-	/**
-	 * @see org.apache.wicket.Application#getHomePage()
-	 */
+
 	@Override
 	public Class<? extends WebPage> getHomePage() {
 		return FormPage.class;
 	}
 
-	/**
-	 * @see org.apache.wicket.Application#init()
-	 */
 	@Override
 	public void init() {
 		super.init();
@@ -31,6 +20,8 @@ public class WicketApplication extends WebApplication {
 		mountPage("/formpage", FormPage.class);
 
 		addResourceReplacement(WiQueryCoreThemeResourceReference.get(),
-				new WiQueryCoreThemeResourceReference("smoothness"));		
+				new WiQueryCoreThemeResourceReference("smoothness"));
+
+		getMarkupSettings().setStripWicketTags(true);
 	}
 }
--- www/wicketapp2/src/main/java/mg/wicketapp2/page/MainPage.java
+++ www/wicketapp2/src/main/java/mg/wicketapp2/page/MainPage.java
@@ -4,14 +4,8 @@ import mg.wicketapp2.panel.mainpage.FooterPanel;
 import mg.wicketapp2.panel.mainpage.HeaderPanel;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.odlabs.wiquery.core.events.Event;
-import org.odlabs.wiquery.core.events.MouseEvent;
-import org.odlabs.wiquery.core.events.WiQueryEventBehavior;
-import org.odlabs.wiquery.core.javascript.JsScope;
-import org.odlabs.wiquery.ui.dialog.Dialog;
 
 public abstract class MainPage extends WebPage {
 
@@ -27,24 +21,6 @@ public abstract class MainPage extends WebPage {
 		add(headerPanel);
 		add(footerPanel);
 		add(feedbackPanel);
-
-		// TODO: remove example dialog usage
-		final Dialog dialog = new Dialog("dialog");
-		add(dialog);
-
-		Button button = new Button("open-dialog");
-		button.add(new WiQueryEventBehavior(new Event(MouseEvent.CLICK) {
-
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public JsScope callback() {
-				return JsScope.quickScope(dialog.open().render());
-			}
-
-		}));
-		add(button);
-
 	}
 
 	public MainPage() {
