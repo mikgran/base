eb58dc8b8ca5349e2780c1d4d149af8376062ac8
Mika Granqvist <mika.granqvist@outlook.com>
2014-06-22 18:56:21 +0300

    Added transition for adding a new reservation. - button - form - response page without model

--- www/wicketapp/src/main/java/mg/reservation/panel/ReservationsPanel.java
+++ www/wicketapp/src/main/java/mg/reservation/panel/ReservationsPanel.java
@@ -7,9 +7,13 @@ import mg.reservation.util.Common;
 
 import org.apache.wicket.ajax.AjaxEventBehavior;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
 import org.apache.wicket.datetime.PatternDateConverter;
 import org.apache.wicket.datetime.markup.html.basic.DateLabel;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.validation.IFormValidator;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.PropertyListView;
 import org.apache.wicket.markup.html.panel.Panel;
@@ -24,12 +28,52 @@ public class ReservationsPanel extends Panel {
 	private Logger logger = LoggerFactory.getLogger(ReservationsPanel.class);
 	private static final long serialVersionUID = 830442203196048049L;
 	private ReservationsModel reservationsModel;
+	private PropertyListView<Reservation> reservationsListView;
+	private Form<Void> addRowForm;
 
 	public ReservationsPanel(String id, ReservationsModel reservationsModel) {
 		super(id);
 		this.reservationsModel = reservationsModel;
 
-		add(getReservationsListView(reservationsModel));
+		reservationsListView = getReservationsListView(reservationsModel);
+		addRowForm = getAddRowForm();
+		addRowForm.add(getAjaxAddReservationSubmitLink());
+
+		add(reservationsListView);
+		add(addRowForm);
+	}
+
+	private AjaxSubmitLink getAjaxAddReservationSubmitLink() {
+
+		return new AjaxSubmitLink("ajaxAddReservation") {
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+
+				logger.debug("AjaxSubmitLink ajaxAddReservation onSubmit()");
+
+				setResponsePage(new ReservationDetailsPage(new PageParameters(),
+						getReservationsModel()));
+			}
+		};
+	}
+
+	private Form<Void> getAddRowForm() {
+		return new Form<Void>("addReservation") {
+
+			private static final long serialVersionUID = -5477559594122177330L;
+
+			@Override
+			public void onSubmit() {
+
+				logger.debug("form addReservation onSubmit()");
+
+				setResponsePage(new ReservationDetailsPage(new PageParameters(),
+						getReservationsModel()));
+			}
+		};
+
 	}
 
 	private PropertyListView<Reservation> getReservationsListView(ReservationsModel reservationsModel) {
@@ -47,7 +91,12 @@ public class ReservationsPanel extends Panel {
 				item.add(new DateLabel("start", new PatternDateConverter(Common.DD_MM_YYYY_HH_MM, false)));
 				item.add(new DateLabel("end", new PatternDateConverter(Common.DD_MM_YYYY_HH_MM, false)));
 
-				item.add(new AjaxEventBehavior("onclick") {
+				item.add(getAjaxEventBehavior(item));
+
+			}
+
+			private AjaxEventBehavior getAjaxEventBehavior(final ListItem<Reservation> item) {
+				return new AjaxEventBehavior("onclick") {
 					private static final long serialVersionUID = 2856171987289507739L;
 
 					@Override
@@ -60,9 +109,9 @@ public class ReservationsPanel extends Panel {
 								new Model<Reservation>(item.getModelObject())));
 					}
 
-				});
-
+				};
 			}
+
 		};
 	}
 
