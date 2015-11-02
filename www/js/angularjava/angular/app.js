5f17b7c7a0d1ca7499b6b1215ad75b572730d137
Mika Granqvist <mika.granqvist@outlook.com>
2015-11-02 19:28:59 +0200

    Removed jade dependencies and usage from app.

--- www/js/angularjava/angular/app.js
+++ www/js/angularjava/angular/app.js
@@ -1,60 +1,79 @@
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.get('/contactlist', function (req, res) {
+    console.log("got GET request");
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+    person1 = {
+        name: 'Timmy',
+        email: 'timmy@email.com',
+        number: '(111) 111-1111'
+    };
+
+    person2 = {
+        name: 'Emily',
+        email: 'emily@email.com',
+        number: '(222) 222-2222'
+    };
+
+    person3 = {
+        name: 'John',
+        email: 'john@email.com',
+        number: '(333) 333-3333'
+    };
+
+    var contactlist = [person1, person2, person3];
+
+    res.json(contactlist);
+
+    // console.log(db);
+
+    /*db.contactlist.find(function (err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });*/
+})
+
+app.post('/contactlist', function (req, res) {
+
+    console.log("got POST request");
+    console.log(req.body);
+
+    /*db.contactlist.insert(req.body, function (err, doc) {
+        res.json(doc);
+    });*/
 });
 
-// error handlers
+app.delete('/contactlist/:id', function (req, res) {
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+    var id = req.params.id;
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+    console.log("got DELETE request: id: " + id);
 
+    /*db.contactlist.remove({
+        _id: mongojs.ObjectId(id),
+        function (err, doc) {
+            console.log('err' + err);
+            res.json(doc);
+        }
+    }, function (err, doc) {
+        res.json(doc);
+    });*/
+
+})
 
 module.exports = app;
