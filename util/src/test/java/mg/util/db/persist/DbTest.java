b4f382a075b4e78426137e4f2a2f856c0feed2b5
Mika Granqvist <mika.granqvist@outlook.com>
2015-11-21 20:14:34 +0200

    Added test coverage to db.remove().

--- util/src/test/java/mg/util/db/persist/DbTest.java
+++ util/src/test/java/mg/util/db/persist/DbTest.java
@@ -24,7 +24,7 @@ public class DbTest {
 
     private static final String TEST_DB_NAME = "dbotest";
     private static final String TEST_DB_TABLE_NAME = "contacts";
-    private static final String TEST_DB_TABLE_NAME2 = "contacts";
+    private static final String TEST_DB_TABLE_NAME2 = "contacts2";
     private static final String SELECT_FROM_CONTACTS_WHERE_ID_IS = "SELECT * FROM contacts WHERE id = %d;";
     private static final String SELECT_FROM_CONTACTS2_WHERE_ID_IS = "SELECT * FROM contacts2 WHERE id = %d;";
     private static final String SELECT_ALL_FROM_CONTACTS = format("SELECT * FROM %s;", TEST_DB_TABLE_NAME);
@@ -109,7 +109,7 @@ public class DbTest {
                 fail("database should contain a contact with id 2");
             }
 
-            assertEquals(format("after save() %s should have id", TEST_DB_TABLE_NAME2), 2, contact2.getId());
+            assertEquals(format("after save() %s should have id", TEST_DB_TABLE_NAME), 2, contact2.getId());
             assertEquals(name2, resultSet2.getString("name"));
             assertEquals(email2, resultSet2.getString("email"));
             assertEquals(phone2, resultSet2.getString("phone"));
@@ -137,19 +137,24 @@ public class DbTest {
         final String email = "name@email.com";
         final String phone = "(111) 111-1111";
 
-        final String name2 = "name2";
-        final String email2 = "name2@email.com";
+        final String name2 = name + "2";
+        final String email2 = email + "2";
         final String phone2 = "(222) 222-2222";
 
-        Contact2 contact = new Contact2(0, name, email, phone);
+        final String name3 = name + "3";
+        final String email3 = email + "3";
+        final String phone3 = "(333) 333-3333";
+
+        Contact2 contact1 = new Contact2(0, name, email, phone);
         Contact2 contact2 = new Contact2(0, name2, email2, phone2);
+        Contact2 contact3 = new Contact2(0, name3, email3, phone3);
         DB db = new DB(connection);
 
         try (Statement statement = connection.createStatement()) {
 
-            db.dropTable(contact);
-            db.createTable(contact);
-            db.save(contact);
+            db.dropTable(contact1);
+            db.createTable(contact1);
+            db.save(contact1);
 
             ResultSet resultSet = statement.executeQuery(format(SELECT_ALL_FROM_CONTACTS2));
             if (!resultSet.next()) {
@@ -157,16 +162,37 @@ public class DbTest {
             }
 
             db.save(contact2);
+
             ResultSet resultSet2 = statement.executeQuery(format(SELECT_FROM_CONTACTS2_WHERE_ID_IS, contact2.getId()));
             if (!resultSet2.next()) {
                 fail("database should contain a contact2 row.");
             }
 
-            db.remove(contact);
+            db.remove(contact1);
 
-            ResultSet resultSet3 = statement.executeQuery(format(SELECT_FROM_CONTACTS2_WHERE_ID_IS, contact.getId()));
+            ResultSet resultSet3 = statement.executeQuery(format(SELECT_FROM_CONTACTS2_WHERE_ID_IS, contact1.getId()));
             if (resultSet3.next()) {
-                fail("database should not contain a row for contact.");
+                fail("database should not contain a row for contact 1.");
+            }
+
+            db.save(contact3);
+
+            ResultSet resultSet4 = statement.executeQuery(format(SELECT_FROM_CONTACTS2_WHERE_ID_IS, contact3.getId()));
+            if (!resultSet4.next()) {
+                fail("database should contain a row for contact 3");
+            }
+
+            assertEquals(format("after save() %s should have id", TEST_DB_TABLE_NAME2), 3, contact3.getId());
+            assertEquals(name3, resultSet4.getString("name"));
+            assertEquals(email3, resultSet4.getString("email"));
+            assertEquals(phone3, resultSet4.getString("phone"));
+
+            db.remove(contact2);
+            db.remove(contact3);
+
+            ResultSet resultSet5 = statement.executeQuery(format(SELECT_ALL_FROM_CONTACTS2));
+            if (resultSet5.next()) {
+                fail("database should not contain any rows after removing all three test contacts.");
             }
         }
 
