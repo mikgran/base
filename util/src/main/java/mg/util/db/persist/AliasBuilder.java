d0bd9a9166603797b37de15e580e3d899062242c
Mika Granqvist <mika.granqvist@outlook.com>
2016-01-12 23:31:44 +0200

    Added AliasBuilder and tests.

--- util/src/main/java/mg/util/db/persist/AliasBuilder.java
+++ util/src/main/java/mg/util/db/persist/AliasBuilder.java
@@ -5,44 +5,133 @@ import static mg.util.validation.Validator.validateNotNullOrEmpty;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+/**
+ * Creates new aliases and caches the created aliases in a LinkedHashMap of LinkedHashMaps.
+ *
+ * The aliases are organized in the following manner: Map<familyKey, Map<memberKey, memberValue>.
+ * The typical contents of an AliasBuilder for example:
+<pre>
+(
+  (
+    "c",
+    (
+      ("contacts", "c1"),
+      ("consumers", "c2"),
+      ("contracts", "c3")
+    )
+  ),
+  (
+    "d",
+    (
+      ("deliveries", "d1")
+    )
+  )
+)
+<pre>
+ */
 public class AliasBuilder {
 
-    Map<String, String> aliases = new LinkedHashMap<>();
+    private Map<String, Map<String, String>> aliases = new LinkedHashMap<>();
 
-    /*
-     * Map<family, members>
+    public int aliasCount() {
+
+        return aliases.entrySet()
+                      .stream()
+                      .map(entry -> entry.getValue())
+                      .map(entry -> entry.size())
+                      .mapToInt(Integer::intValue)
+                      .sum();
+    }
+
+    /**
+     * Builds a new SQL alias for a string and caches the alias
+     * internally. Uses the first character of the provided String
+     * to create the alias.
+     *
+     * @param string the String to create the alias for.
+     * Must be not null and non empty.
+     * @return the new alias
      */
+    public String aliasOf(String string) {
 
-
-    public String aliasTable(String tableName) {
-
-        validateNotNullOrEmpty("tableName", tableName);
+        validateNotNullOrEmpty("tableName", string);
 
         // starting letter
         // find out current highest index
         // increment and combine
 
-        String firstChar = String.valueOf(tableName.charAt(0));
+        String firstChar = firstCharacterOf(string);
+        String newAlias = "";
 
+        Map<String, String> familyMap = aliases.get(firstChar);
 
+        if (familyMap != null) {
 
-        return "";
+            newAlias = buildAlias(firstChar, familyMap);
+            familyMap.put(string, newAlias);
+
+        } else {
+
+            Map<String, String> newFamilyMap = new LinkedHashMap<>();
+            newAlias = buildAlias(firstChar, newFamilyMap);
+            newFamilyMap.put(string, newAlias);
+            aliases.put(firstChar, newFamilyMap);
+        }
+
+        return newAlias;
     }
 
-    public Map<String, String> getAliases() {
-        return aliases;
-    }
-
+    // Map<familyKey, LinkedHashMap<memberKey, member>>
     public void clear() {
+        aliases.entrySet()
+               .stream()
+               .forEach(entry -> entry.getValue()
+                                      .clear());
         aliases.clear();
     }
 
-    public int aliasCount() {
+    // Map<familyKey, LinkedHashMap<memberKey, member>>
+    public int familyCount() {
         return aliases.size();
     }
 
+    public String firstCharacterOf(String string) {
+        if (string != null && string.length() > 0) {
+            return String.valueOf(string.charAt(0));
+        }
+        return "";
+    }
+
+    /**
+     * Returns an alias for the provided key.
+     * @param key The key to use in fetching the alias.
+     * @return Returns the alias corresponding the key, or null if no matching alias is found.
+     */
     public String getAlias(String key) {
-        return aliases.get(key);
+
+        if (key != null && key.length() > 0) {
+
+            Map<String, String> familyMap = aliases.get(firstCharacterOf(key));
+
+            if (familyMap != null) {
+
+                return familyMap.get(key);
+            }
+        }
+        return null;
+    }
+
+    //    public Map<String, String> getAliases(String familyKey) {
+    //
+    //        if (familyKey != null && familyKey.length() > 0) {
+    //            return Collections.emptyMap();
+    //        }
+    //
+    //        return Collections.emptyMap();
+    //    }
+
+    private String buildAlias(String charAsString, Map<String, String> familyMap) {
+        return charAsString + (familyMap.size() + 1);
     }
 
 }
--- util/src/test/java/mg/util/db/persist/AliasBuilderTest.java
+++ util/src/test/java/mg/util/db/persist/AliasBuilderTest.java
@@ -13,18 +13,29 @@ public class AliasBuilderTest {
 
         AliasBuilder aliasBuilder = new AliasBuilder();
 
-        String contactsAliasCandidate = aliasBuilder.aliasTable("contacts");
+        String contactsAliasCandidate = aliasBuilder.aliasOf("contacts");
 
         assertNotNull(contactsAliasCandidate);
         assertEquals("alias for contacts should be: ", "c1", contactsAliasCandidate);
+        assertEquals("there should be alias families: ", 1, aliasBuilder.familyCount());
         assertEquals("there should be aliases: ", 1, aliasBuilder.aliasCount());
         assertEquals("builder should have an alias for contacts: ", "c1", aliasBuilder.getAlias("contacts"));
 
-        String consumerAliasCandidate = aliasBuilder.aliasTable("consumer");
+        String consumerAliasCandidate = aliasBuilder.aliasOf("consumers");
         assertNotNull(consumerAliasCandidate);
         assertEquals("alias for consumer should be: ", "c2", consumerAliasCandidate);
+        assertEquals("there should be alias families: ", 1, aliasBuilder.familyCount());
         assertEquals("there should be aliases: ", 2, aliasBuilder.aliasCount());
-        assertEquals("builder should have an alias for consumers: ", "c2", aliasBuilder.getAlias("consumer"));
         assertEquals("builder should have an alias for contacts still: ", "c1", aliasBuilder.getAlias("contacts"));
+        assertEquals("builder should have an alias for consumers: ", "c2", aliasBuilder.getAlias("consumers"));
+
+        String deliveriesAliasCandidate = aliasBuilder.aliasOf("deliveries");
+        assertNotNull(deliveriesAliasCandidate);
+        assertEquals("alias for deliveries should be: ", "d1", deliveriesAliasCandidate);
+        assertEquals("there should be alias families: ", 2, aliasBuilder.familyCount());
+        assertEquals("there should be aliases: ", 3, aliasBuilder.aliasCount());
+        assertEquals("builder should have an alias for contacts still: ", "c1", aliasBuilder.getAlias("contacts"));
+        assertEquals("builder should have an alias for consumers: ", "c2", aliasBuilder.getAlias("consumers"));
+        assertEquals("builder should have an alias for consumers: ", "d1", aliasBuilder.getAlias("deliveries"));
     }
 }
