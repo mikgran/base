f543516a4c302bf5f243ffa2c34781d4de7a634e
Mika Granqvist <mika.granqvist@outlook.com>
2016-02-11 01:27:25 +0200

    Changed parameter object to use final fields.

--- util/src/main/java/mg/util/db/persist/SqlByFieldsParameters.java
+++ util/src/main/java/mg/util/db/persist/SqlByFieldsParameters.java
@@ -2,22 +2,21 @@ package mg.util.db.persist;
 
 public class SqlByFieldsParameters {
 
-    public StringBuilder byFieldsSql;
-    public String constraints;
-    public String fieldNames;
-    public String joins;
-    public String tableNameAlias;
-
-    public SqlByFieldsParameters(String fieldNames, String joins, String constraints, String tableNameAlias) {
-        this.fieldNames = fieldNames;
-        this.joins = joins;
-        this.constraints = constraints;
-        this.tableNameAlias = tableNameAlias;
-    }
+    public final String constraints;
+    public final String fieldNames;
+    public final String joins;
+    public final String tableNameAlias;
 
     public SqlByFieldsParameters(String fieldNames,  String constraints, String tableNameAlias) {
         this.fieldNames = fieldNames;
         this.joins = "";
+        this.constraints = constraints;
+        this.tableNameAlias = tableNameAlias;
+    }
+
+    public SqlByFieldsParameters(String fieldNames, String joins, String constraints, String tableNameAlias) {
+        this.fieldNames = fieldNames;
+        this.joins = joins;
         this.constraints = constraints;
         this.tableNameAlias = tableNameAlias;
     }
