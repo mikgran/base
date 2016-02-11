558bc01817f8b7076df8450c44e05015da1460dc
Mika Granqvist <mika.granqvist@outlook.com>
2016-02-11 13:27:43 +0200

    Fixed a test.

--- util/src/test/java/mg/util/db/persist/proxy/ListProxyTest.java
+++ util/src/test/java/mg/util/db/persist/proxy/ListProxyTest.java
@@ -1,5 +1,7 @@
 package mg.util.db.persist.proxy;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,7 @@ import org.junit.rules.ExpectedException;
 import mg.util.db.TestDBSetup;
 import mg.util.db.persist.DB;
 import mg.util.db.persist.support.Person3;
+import mg.util.db.persist.support.Todo;
 import mg.util.db.persist.support.Todo3;
 
 public class ListProxyTest {
@@ -32,27 +35,25 @@ public class ListProxyTest {
 
         DB db = new DB(connection);
 
-        ListProxyParameters<List<Todo3>> listProxyParameters = new ListProxyParameters<List<Todo3>>(db, new ArrayList<Todo3>());
+        ArrayList<Todo3> todoList = new ArrayList<Todo3>();
+        todoList.add(new Todo3(TEST_VALUE));
+        ListProxyParameters<List<Todo3>> listProxyParameters = new ListProxyParameters<List<Todo3>>(db, todoList);
 
         List<Todo3> proxyList = ListProxy.newInstance(listProxyParameters);
 
-        Person3 person3 = new Person3("first1", "last2", proxyList);
+        assertEquals("proxy list should have the size of:", 1, proxyList.size());
+        assertEquals("proxy list get(0).getFirstName should be: ", TEST_VALUE, proxyList.get(0).getTodo());
 
-//        list = ListProxy.newInstance(listProxParams, stringListProcessor);
-//
-//        assertEquals("proxy list should have the size of:", 1, list.size());
-//        assertEquals("proxy list should contain: ", TEST_VALUE, list.get(0).getFirstName());
-//
-//        list.add(TEST_VALUE + "2");
-//
-//        assertEquals("proxy list should have the size of: ", 2, list.size());
-//        assertEquals("proxy list should contain: ", TEST_VALUE + "2", list.get(1));
-//
-//        String s = list.stream()
-//                       .reduce("", (a, b) -> a + b);
-//
-//        assertEquals("after reduction of the list test, the string should be: ", TEST_VALUE + TEST_VALUE + "2", s);
-//
+        proxyList.add(new Todo3(TEST_VALUE + "2"));
+
+        assertEquals("proxy list should have the size of: ", 2, proxyList.size());
+        assertEquals("proxy list should contain: ", TEST_VALUE + "2", proxyList.get(1).getTodo());
+
+        String s = proxyList.stream()
+                            .map(todo -> todo.getTodo())
+                            .reduce("", (a, b) -> a + b);
+
+        assertEquals("after reduction of the list test, the string should be: ", TEST_VALUE + TEST_VALUE + "2", s);
     }
 
 }
