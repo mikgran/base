c13e3a152a26c83895ec015bf514b680b289806e
Mika Granqvist <mika.granqvist@outlook.com>
2016-02-16 14:27:06 +0200

    Lazy fetch changes.

--- util/src/main/java/mg/util/db/persist/ResultSetLazyMapper.java
+++ util/src/main/java/mg/util/db/persist/ResultSetLazyMapper.java
@@ -3,7 +3,10 @@ package mg.util.db.persist;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
+
+import mg.util.db.persist.field.FieldBuilder;
 
 public class ResultSetLazyMapper<T extends Persistable> extends ResultSetMapper<T> {
 
@@ -27,19 +30,30 @@ public class ResultSetLazyMapper<T extends Persistable> extends ResultSetMapper<
         return newType;
     }
 
-    @SuppressWarnings("unused")
+    // @SuppressWarnings("unused")
     private void buildAndAssignProxies(ResultSet resultSet, T newType, T refType) throws DBValidityException {
 
-        SqlBuilder newTypeBuilder = SqlBuilderFactory.of(newType);
+        // SqlBuilder newTypeBuilder = SqlBuilderFactory.of(newType);
 
-        // 1. assign proxy lists with parameters (including fetch by referring idsl)
+        // 1. assign proxy lists with parameters (including fetch by referring ids)
         // 2. assign OneToMany proxies for each non null collection that has at least one element
         // 3. assign OneToOne proxies for each non null oneToOne
 
-        Collection<Persistable> uniqueCollectionTypes;
-        uniqueCollectionTypes = refSqlBuilder.getReferenceCollectionPersistables(refType)
-                                             .collect(Collectors.toMap(Persistable::getClass, p -> p, (p, q) -> p))
-                                             .values();
+        //        Collection<Persistable> uniqueCollectionTypes;
+        //        uniqueCollectionTypes = refSqlBuilder.getReferenceCollectionPersistables(refType)
+        //                                             .collect(Collectors.toMap(Persistable::getClass, p -> p, (p, q) -> p))
+        //                                             .values();
+        //
+        //        uniqueCollectionTypes.forEach(colType -> System.out.println(colType));
+
+        refSqlBuilder.getOneToManyBuilders()
+                     .stream()
+                     .map(colBuilder -> colBuilder.getFieldValue(refType))
+                     .filter(object -> object != null && object instanceof Collection<?>)
+                     .map(object -> (Collection<?>) object)
+                     .filter(collection -> collection.size() > 0)
+                     ;
 
     }
+
 }
