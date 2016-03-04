0fa0494e89cd1c7f393c3a6e46d998c3a0868619
Mika Granqvist <mika.granqvist@outlook.com>
2016-03-04 09:51:48 +0200

    Code cleanup.

--- util/src/test/java/mg/util/db/persist/ResultSetMapperTest.java
+++ util/src/test/java/mg/util/db/persist/ResultSetMapperTest.java
@@ -78,11 +78,9 @@ public class ResultSetMapperTest {
         assertNotNull(locations);
         assertTrue("locations should be an instance of ListProxy<?>: ", locations.getClass().getCanonicalName().contains("Proxy"));
 
-        // case ListProxy -> locations XXX
         assertEquals("the size of locations should be: ", 1, locations.size()); // force pull from db.
         assertTrue("there should be a location5 with id greater than 0: ", locations.get(0).getId() > 0);
         assertEquals("the person5id should be: ", person5.getId(), locations.get(0).getPersonsId());
-
     }
 
     // mvn -DfailIfNoTests=false -Dtest=ResultSetMapperTest#testMappingOne test
