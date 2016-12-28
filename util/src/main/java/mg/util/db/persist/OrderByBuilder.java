d859e6ba851bf9e31f629d868b5d2f56af1e739a
Mika Granqvist <mika.granqvist@outlook.com>
2016-12-28 20:26:34 +0200

    Added missing file.

---
+++ util/src/main/java/mg/util/db/persist/OrderByBuilder.java
@@ -0,0 +1,39 @@
+package mg.util.db.persist;
+
+import static mg.util.validation.Validator.validateNotNull;
+
+public class OrderByBuilder {
+
+    public enum Direction {
+        ASC, DESC;
+    }
+
+    private String fieldName;
+    private Direction direction;
+
+    public OrderByBuilder(String fieldName) {
+        this(fieldName, Direction.ASC);
+    }
+
+    public OrderByBuilder(String fieldName, Direction direction) {
+        validateNotNull("fieldName", fieldName);
+        this.fieldName = fieldName;
+        this.direction = direction != null ? direction : Direction.ASC;
+    }
+
+    public String build() {
+        return new StringBuffer().append("ORDER BY ")
+                                 .append(fieldName)
+                                 .append(" ")
+                                 .append(direction.toString())
+                                 .toString();
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+}
