bc45240e6a63468ffb628edc5e2e1b667c1304bd
Mika Granqvist <mika.granqvist@outlook.com>
2017-01-23 23:09:19 +0200

    Added conveniency methods.

--- util/src/main/java/mg/util/validation/rule/ValidationRule.java
+++ util/src/main/java/mg/util/validation/rule/ValidationRule.java
@@ -14,9 +14,33 @@ public abstract class ValidationRule {
     public static final ContainsFieldRule CONTAINS_FIELD = new ContainsFieldRule();
     public static final FieldTypeMatchesRule FIELD_TYPE_MATCHES = new FieldTypeMatchesRule();
 
-    public abstract boolean apply(Object object);
+    public static ConnectionNotClosedRule connectionNotClosed() {
+        return CONNECTION_NOT_CLOSED;
+    }
 
-    public abstract String getMessage();
+    public static ContainsFieldRule containsField() {
+        return CONTAINS_FIELD;
+    }
+
+    public static DateConstraintRule dateConstraint() {
+        return DATE_CONSTRAINT;
+    }
+
+    public static DateEarlierRule dateEarlier() {
+        return DATE_EARLIER;
+    }
+
+    public static FieldTypeMatchesRule fieldTypeMatches() {
+        return FIELD_TYPE_MATCHES;
+    }
+
+    public static NotNegativeOrZeroNumberRule notNegativeOrZero() {
+        return NOT_NEGATIVE_OR_ZERO;
+    }
+
+    public static NotNegativeOrZeroStringAsNumberRule notNegativeOrZeroAsString() {
+        return NOT_NEGATIVE_OR_ZERO_AS_STRING;
+    }
 
     // TOCONSIDER: moving most common rules into Validator or creating methods for each rule in the abstract class.
     // Note to self: all capitals look hor-ri-bad. They just do.
@@ -27,4 +51,12 @@ public abstract class ValidationRule {
     public static ValidationRule notNullOrEmptyString() {
         return NOT_NULL_OR_EMPTY_STRING;
     }
+
+    public static NumberOfCharactersRule numberOfCharacters() {
+        return NUMBER_OF_CHARACTERS;
+    }
+
+    public abstract boolean apply(Object object);
+
+    public abstract String getMessage();
 }
