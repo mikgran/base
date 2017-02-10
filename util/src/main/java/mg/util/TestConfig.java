ca9152dda4b704e90bd20f51dafdde6b502a577b
Mika Granqvist <mika.granqvist@outlook.com>
2017-02-10 16:29:57 +0200

    Fixed TestConfig for reservations.

--- util/src/main/java/mg/util/TestConfig.java
+++ util/src/main/java/mg/util/TestConfig.java
@@ -3,12 +3,25 @@ package mg.util;
 import java.io.IOException;
 import java.util.Properties;
 
+import mg.util.db.DBConfig;
+
 public class TestConfig extends Config {
+
+    private String dbName = null;
+
+    public TestConfig() {
+    }
+
+    public TestConfig(String dbName) {
+        this.dbName = dbName;
+    }
 
     @Override
     public Properties loadProperties() throws IOException {
         Properties properties = loadProperties("test-config.properties");
+        if (Common.hasContent(dbName)) {
+            properties.setProperty(DBConfig.DB_URL, dbName);
+        }
         return properties;
     }
 }
-
