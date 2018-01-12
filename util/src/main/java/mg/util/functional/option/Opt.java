11c367202e1a75edd884086cc951b0bcf0830d7b
Mika Granqvist <mika.granqvist@outlook.com>
2018-01-12 18:12:45 +0200

    Added comments.

--- util/src/main/java/mg/util/functional/option/Opt.java
+++ util/src/main/java/mg/util/functional/option/Opt.java
@@ -22,26 +22,24 @@ import mg.util.validation.Validator;
 /**
  * A value container class for dealing with the non-null paradigm, to promote
  * the use of functional programming approach and to help dealing with the absence of
- * a value by offering tools to alter the program flow without the need
+ * a value by offering tools to alter the program flow without the need to
  * continuously write code to inspect whether the value is present or not.<br /><br />
  *
- * To test the equality on the objects of this class, test the equality on the contents by using
- * Opt.get().equals(Opt.get()). Testing equality on the containers with same contents will result in false.
- * i.e. Opt.of("val1").equals(Opt.of("val1")) == false.<br /><br />
+ * The equals of this class is performed on the contents: Opt.of("value").equals(Of.of("value")) == true.
  *
  * All filtering, mapping, and matching methods are content based. Methods are executed if isPresent()
  * evaluates true. All methods return the original or transformed value in a new Opt.of(transformedValue) or
  * BiOpt.of(originalValue, transformedValue) for easy chaining of methods.<br /><br />
  *
- * If any transformation or filtering method results in null value the Opt.empty() is returned. <br /><br />
+ * If any construction, transformation or filtering method results in null value the Opt.empty() is returned. <br /><br />
  *
  * <pre>
- * For instance, following call chain executes fully:
+ * For instance, the following call chain executes fully:
  *      Opt.of("value1")
  *         .map(s -> s + "2")
  *         .get()
  *
- *      And is equal to:
+ * The call chain is equal to:
  *
  *      Opt.of("value2")
  *         .get()
@@ -54,7 +52,7 @@ import mg.util.validation.Validator;
  *         .map(s -> s + "2")
  *         .get()
  *
- *      And is equal to:
+ * The call chain is equal to:
  *
  *      Opt.empty() or ((T)null);
  * </pre>
@@ -64,20 +62,35 @@ public class Opt<T> {
     private static final Opt<?> EMPTY = new Opt<>();
     private final T value;
 
+    /**
+     * Creates an empty value container of type T.
+     */
     public static <T> Opt<T> empty() {
         @SuppressWarnings("unchecked")
         Opt<T> t = (Opt<T>) EMPTY;
         return t;
     }
 
+    /**
+     * Creates a value container from an existing container.
+     * If the opt contains no value an Opt.empty is returned
+     */
     public static <T> Opt<T> of(Opt<T> opt) {
         return opt == null || !opt.isPresent() ? empty() : new Opt<>(opt.get());
     }
 
+    /**
+     * Creates a value container from an existing optional.
+     * If the optional contains no value an Opt.empty is returned.
+     */
     public static <T> Opt<T> of(Optional<T> optional) {
         return optional == null || !optional.isPresent() ? empty() : new Opt<>(optional.get());
     }
 
+    /**
+     * Creates a value container from the value.
+     * If the value is null an Opt.empty is returned.
+     */
     public static <T> Opt<T> of(T value) {
         return value == null ? empty() : new Opt<>(value);
     }
@@ -90,6 +103,9 @@ public class Opt<T> {
         this.value = value;
     }
 
+    /**
+     * Performs an equals of the value of this object and the obj value.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -104,6 +120,10 @@ public class Opt<T> {
         return Objects.equals(value, other.value);
     }
 
+    /**
+     * Performs a test of the predicate if the value is present. If the predicate
+     * evaluates true this is returned otherwise an Opt.empty is returned.
+     */
     public Opt<T> filter(Predicate<? super T> predicate) {
         Validator.validateNotNull("predicate", predicate);
         if (!isPresent()) {
@@ -113,10 +133,19 @@ public class Opt<T> {
         }
     }
 
+    /**
+     * Performs an predicate.test(value) if value is present. If the predicate
+     * returns true returns this otherwise an Opt.empty is returned.
+     * The predicate is assumed to be able to throw an Exception while testing.
+     */
     public <X extends Exception> Opt<T> filter(ThrowingPredicate<? super T, X> throwingPredicate) throws X {
         return filter(predicateOf(throwingPredicate));
     }
 
+    /**
+     * Performs a transformation on the value. If the value would be return an Opt.of(value)
+     * The value is not re-wrapped in a new Opt. If the result is null an Opt.empty is returned.
+     */
     public <U> Opt<U> flatMap(Function<? super T, Opt<U>> mapper) {
         Validator.validateNotNull("mapper", mapper);
         if (!isPresent()) {
@@ -130,14 +159,27 @@ public class Opt<T> {
         }
     }
 
+    /**
+     * Performs a transformation of the value if it is present. If the transformation would
+     * return an Opt.of(value) the value is not re-wrapped in a new Opt. If the result is a null an
+     * Opt.empty is returned. The transforming function is assumed to be able to throw an
+     * Exception during the transformation.
+     */
     public <X extends Exception, U> Opt<U> flatMap(ThrowingFunction<? super T, Opt<U>, X> mapper) throws X {
         return flatMap(functionOf(mapper));
     }
 
+    /**
+     * Returns the contents of this value container. Returned value may be null.
+     */
     public T get() {
         return value;
     }
 
+    /**
+     * Performs a transformation of the value if it is present and returns the
+     * result of that function. Returned value may be null.
+     */
     public <U> U getAndMap(Function<? super T, ? extends U> mapper) {
         Validator.validateNotNull("mapper", mapper);
         if (isPresent()) {
@@ -147,23 +189,47 @@ public class Opt<T> {
         }
     }
 
+    /**
+     * Performs a transformation of the value if the it is present and returns the
+     * result of that function. Returned value may be null.
+     * The transforming function is assumed to be able to throw an Exception during the transformation.
+     */
     public <U, X extends Exception> U getAndMap(ThrowingFunction<? super T, ? extends U, X> mapper) throws X {
         return getAndMap(functionOf(mapper));
     }
 
+    /**
+     * Returns the contents of this value container or the other if the value is not present.
+     * Returned value may be null.
+     */
     public T getOrElse(T other) {
         return value != null ? value : other;
     }
 
+    /**
+     * Returns the contents of this value container or the value of the supplier if the value is
+     * not present. Returned value may be null. If the supplier is null, a IllegalArgumentException
+     * is raised.
+     */
     public T getOrElseGet(Supplier<? extends T> supplier) {
         Validator.validateNotNull("supplier", supplier);
         return value != null ? value : supplier.get();
     }
 
+    /**
+     * Returns the contents of this value container or the value of the supplier if the value is
+     * not present. Returned value may be null. If the supplier is null, an IllegalArgumentException
+     * is thrown. The supplier is assumed to be able to throw an Exception during the supplying.
+     */
     public <X extends Exception> T getOrElseGet(ThrowingSupplier<? extends T, X> supplier) throws X {
         return getOrElseGet(supplierOf(supplier));
     }
 
+    /**
+     * Returns the content of this value container or throws an Exception supplied by the exceptionSupplier
+     * if the value is not present. If the exceptionSupplier is null or returns null exception
+     * an IllegalArgumentException is thrown.
+     */
     public <X extends Exception> T getOrElseThrow(Supplier<X> exceptionSupplier) throws X {
         Validator.validateNotNull("exceptionSupplier", exceptionSupplier);
         if (value != null) {
@@ -173,6 +239,12 @@ public class Opt<T> {
         }
     }
 
+    /**
+     * Returns the content of this value container or throws an Exception supplied by the exceptionSupplier
+     * if the value is not present. If the exceptionSupplier is null or returns null exception
+     * an IllegalArgumentException is thrown. The exceptionSupplier is assumed to be able to throw an
+     * Exception during the supplying.
+     */
     public <X extends Exception> T getOrElseThrow(ThrowingSupplier<X, X> exceptionSupplier) throws X {
         return getOrElseThrow(supplierOf(exceptionSupplier));
     }
@@ -182,6 +254,10 @@ public class Opt<T> {
         return Objects.hashCode(value);
     }
 
+    /**
+     * If value is not present supplies a new value. If the supplier is null an IllegalArgumentException is
+     * thrown.
+     */
     public Opt<T> ifEmpty(Supplier<T> supplier) {
         Validator.validateNotNull("supplier", supplier);
         if (value == null) {
@@ -190,10 +266,18 @@ public class Opt<T> {
         return this;
     }
 
+    /**
+     * If value is not present supplies a new value. If the supplier is null an IllegalArgumentException is
+     * thrown. The throwingSupplier is assumed to be able to throw an Exception during supplying.
+     */
     public <X extends Exception> Opt<T> ifEmpty(ThrowingSupplier<T, X> throwingSupplier) throws X {
         return ifEmpty(supplierOf(throwingSupplier));
     }
 
+    /**
+     * Throws an exception provided by the exceptionSupplier if no value is present. If the exceptionSupplier
+     * or the value supplied is null an IllegalArgumentException is thrown.
+     */
     public <X extends Exception> Opt<T> ifEmptyThrow(Supplier<X> exceptionSupplier) throws X {
         Validator.validateNotNull("exceptionSupplier", exceptionSupplier);
         if (value == null) {
@@ -298,6 +382,11 @@ public class Opt<T> {
         return BiOpt.of(value, newRight.get());
     }
 
+    /**
+     * Performs a pattern match. If value.getClass() == matchingValue.getClass() and the value is equal to
+     * matchingValue the matchingMapper is applied and the result is stored in a new BiOpt.right.
+     * If no match is found the BiOpt.right is set as empty. The value is stored in the BiOpt.left.
+     */
     public <V extends Object, X extends Exception> Opt<T> match(V matchingValue, ThrowingConsumer<V, X> matchingConsumer) throws X {
         return match(matchingValue, consumerOf(matchingConsumer));
     }
